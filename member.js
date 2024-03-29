7190013efa7d7060f7627af7bffc957380806891
NicolasKalaydjian <nkalaydjian@frba.utn.edu.ar>
2024-03-29 02:23:50 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillsMember() {
+    // Get the member's skills
+    var memberSkills = getMemberSkills();
+    // Get the member's level
+    var memberLevel = getMemberLevel();
+    // Get the member's name
+    var memberName = getMemberName();
+    // Get the member's age
+    var memberAge = getMemberAge();
+    // Get the member's location
+    var memberLocation = getMemberLocation();
+}
